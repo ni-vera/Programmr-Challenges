fdc13b86ba6cc88008ce0a1044a6e38384d5f632
Nicolás Vera <nicolasveca@hotmail.com>
2023-02-28 16:49:03 +0100

    Primer reto realizado

---
+++ Challenge1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Challenge1
+{
+    internal class Program
+    {
+        public static string[] ReverseArray(string[] strings)
+        {
+            int iterations = strings.Length/2;
+            for(int i = 0; i < iterations; i++)
+            {
+                string primer_int = strings[i];
+                strings[i] = strings[strings.Length - 1 - i];
+                strings[strings.Length - 1 - i] = primer_int;
+
+            }
+            return strings;
+        }
+        static void Main(string[] args)
+        {
+            string[] prueba = { "3", "2", "1" };
+            ReverseArray(prueba);
+            Console.WriteLine(string.Join(" ",prueba));
+        }
+    }
+}
