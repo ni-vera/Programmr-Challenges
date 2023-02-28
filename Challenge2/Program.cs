835fc01fc225da5c0c768613b3a69db388c86017
Nicolás Vera <nverac@uoc.edu>
2023-02-28 18:04:45 +0100

    Segundo reto realizado

---
+++ Challenge2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Challenge2
+{
+    internal class Program
+    {
+        public static int OddSum(int limit)
+        {
+            int n = limit / 2;
+            return Convert.ToInt32(Math.Pow(2,n))+2*(n-1) + n;
+        }
+
+        static void Main(string[] args)
+        {
+            string entrada_usuario = "a";
+            while (!Regex.IsMatch(entrada_usuario,@"\d+")) 
+            { 
+            entrada_usuario = Console.ReadLine();
+            }
+            int entrada_convertida = int.Parse(entrada_usuario);
+            string output = OddSum(entrada_convertida).ToString();
+            Console.WriteLine(output);
+        }
+    }
+}
